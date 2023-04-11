9612ab63531e12acceee02441d2f66d88d399fd0
AbolfazlKhosravi <abolfazlkhosravi.2003jun@gmail.com>
2023-04-11 23:18:37 +0430

    createDropdownHeader

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,20 @@
-import { FaHome, FaCaretDown } from "react-icons/fa";
-import { FiSun, FiMoon, FiAlignRight, FiX } from "react-icons/fi";
+import {
+  FaHome,
+  FaCaretDown,
+  FaTelegram,
+  FaProductHunt,
+  FaBtc,
+  FaInfoCircle,
+  FaInstagramSquare,
+} from "react-icons/fa";
+import {
+  HiNewspaper,
+  HiAcademicCap,
+  HiChartBar,
+  HiChatBubbleLeftRight,
+} from "react-icons/hi2";
+import { BiAnalyse } from "react-icons/bi";
+import { FiSun, FiMoon, FiAlignRight, FiX, FiArrowLeft } from "react-icons/fi";
 
 import { useEffect, useState } from "react";
 import { Switch } from "@headlessui/react";
@@ -7,6 +22,15 @@ import { Switch } from "@headlessui/react";
 const Header = () => {
   const [them, setThem] = useState("white");
   const [dropshot, setDropshot] = useState(false);
+  const [dropdownLearn, setDropdownLearn] = useState(false);
+  const [dropdownContactUs, setDropdownContactUs] = useState(false);
+  const [showFlash, setShowFlash] = useState({
+    instagram: false,
+    telegram: false,
+    aboutMe: false,
+    freeLearn: false,
+    products: false,
+  });
   useEffect(() => {
     const handleResize = () => {
       if (window.innerWidth > 1155) {
@@ -32,12 +56,19 @@ const Header = () => {
     }
   }, [dropshot]);
   return (
-    <div className="backdrop-blur-2xl blur-0 opacity-100 sticky top-0 w-full  flex items-center justify-between p-4 max-[495px]:px-1 ">
-      <Dropshot setDropshot={setDropshot} dropshot={dropshot} />
-      <div className="flex items-center justify-between">
+    <div className="backdrop-blur-2xl blur-0 opacity-100 sticky top-0 w-full  flex items-center justify-between px-4  max-[495px]:px-1 h-16 ">
+      <Dropshot
+        setDropshot={setDropshot}
+        dropshot={dropshot}
+        setDropdownLearn={setDropdownLearn}
+        setDropdownContactUs={setDropdownContactUs}
+        dropdownLearn={dropdownLearn}
+        dropdownContactUs={dropdownContactUs}
+      />
+      <div className="flex items-center justify-between h-full">
         <FiAlignRight
           onClick={() => setDropshot(true)}
-          className="min-[1155px]:hidden text-[2.3rem] ml-4 text-slate-600 max-[495px]:text-[1.9rem]"
+          className="min-[1155px]:hidden text-[2.3rem] ml-4 text-slate-600 max-[495px]:text-[1.9rem] cursor-pointer "
         />
         <div className="flex items-center justify-between ml-8">
           <FaHome className="max-[495px]:text-[1.9rem] text-[2.3rem] text-blue-700" />
@@ -51,8 +82,8 @@ const Header = () => {
             </p>
           </div>
         </div>
-        <ul className="text-[1.1rem] max-[1155px]:hidden flex text-slate-600 ">
-          <li className="px-3 py-1 mx-1 cursor-pointer hover:text-blue-700">
+        <ul className="text-[1.1rem] max-[1155px]:hidden flex items-center text-slate-600 max-h-full min-h-full ">
+          <li className="px-3 py-1 mx-1 cursor-pointer hover:text-blue-700 ">
             اخبار
           </li>
           <li className="px-3 py-1 mx-1 cursor-pointer hover:text-blue-700">
@@ -64,21 +95,192 @@ const Header = () => {
           <li className="px-3 py-1 mx-1 cursor-pointer hover:text-blue-700">
             NFT & Airdrop
           </li>
-          <li className="px-3 py-1 mx-1 cursor-pointer hover:text-blue-700">
+          <li
+            className="px-3 py-5 mx-1 cursor-pointer hover:text-blue-700"
+            onMouseEnter={() => setDropdownLearn(true)}
+            onMouseLeave={() => {
+              setDropdownLearn(false);
+              setShowFlash({
+                ...showFlash,
+                aboutMe: false,
+                instagram: false,
+                telegram: false,
+                products: false,
+                freeLearn: false,
+              });
+            }}
+          >
             <div className="flex items-center justify-between">
               <h2>اموزش</h2>
-              <FaCaretDown className="text-sm mr-2" />
+              <FaCaretDown
+                className={`${dropdownLearn ? " rotate-180 " : ""}text-sm mr-2`}
+              />
+            </div>
+            <div
+              className={`${
+                dropdownLearn ? "  w-96 h-50" : " hidden"
+              } bg-white drop-shadow-xl rounded-b-md absolute translate-x-2 translate-y-[1.1rem] p-2`}
+            >
+              <div
+                className="flex justify-between items-center py-4 px-2 text-slate-600 w-full hover:bg-stone-100 rounded-lg"
+                onMouseEnter={() =>
+                  setShowFlash({
+                    ...showFlash,
+                    aboutMe: false,
+                    instagram: false,
+                    telegram: false,
+                    products: false,
+                    freeLearn: true,
+                  })
+                }
+              >
+                <div className="flex justify-between items-center ">
+                  <FaInfoCircle className="text-xl ml-3 " />
+                  <p className=""> اموزش های رایگان </p>
+                </div>
+                <FiArrowLeft
+                  className={`${
+                    showFlash.freeLearn
+                      ? "opacity-100 animate-arrow-left"
+                      : "opacity-0"
+                  } text-blue-700 `}
+                />
+              </div>
+              <div
+                className="flex justify-between items-center py-4 px-2 text-slate-600 w-full hover:bg-stone-100 rounded-lg"
+                onMouseEnter={() =>
+                  setShowFlash({
+                    ...showFlash,
+                    aboutMe: false,
+                    instagram: false,
+                    telegram: false,
+                    products: true,
+                    freeLearn: false,
+                  })
+                }
+              >
+                <div className="flex justify-between items-center ">
+                  <FaProductHunt className="text-xl ml-3" />
+                  <p className="">صفحه محصولات</p>
+                </div>
+                <FiArrowLeft
+                  className={`${
+                    showFlash.products
+                      ? "opacity-100 animate-arrow-left"
+                      : "opacity-0"
+                  } text-blue-700  `}
+                />
+              </div>
             </div>
           </li>
-          <li className="px-3 py-1 mx-1 cursor-pointer hover:text-blue-700">
-            <div className="flex items-center justify-between">
+          <li
+            className="px-3 py-5 mx-1 cursor-pointer hover:text-blue-700   "
+            onMouseEnter={() => setDropdownContactUs(true)}
+            onMouseLeave={() => {
+              setDropdownContactUs(false);
+              setShowFlash({
+                ...showFlash,
+                aboutMe: false,
+                instagram: false,
+                telegram: false,
+                products: false,
+                freeLearn: false,
+              });
+            }}
+          >
+            <div className="flex items-center justify-between ">
               <h2> ارتباط با ما</h2>
-              <FaCaretDown className="text-sm mr-2" />
+              <FaCaretDown
+                className={`${
+                  dropdownContactUs ? " rotate-180 " : ""
+                }text-sm mr-2`}
+              />
+            </div>
+            <div
+              className={`${
+                dropdownContactUs ? "  w-80 h-50" : " hidden"
+              } bg-white drop-shadow-xl rounded-b-md absolute translate-x-2 translate-y-[1.1rem] p-2`}
+            >
+              <div
+                className="flex justify-between items-center py-4 px-2 text-slate-600 w-full hover:bg-stone-100 rounded-lg"
+                onMouseEnter={() =>
+                  setShowFlash({
+                    ...showFlash,
+                    aboutMe: true,
+                    instagram: false,
+                    telegram: false,
+                    products: false,
+                    freeLearn: false,
+                  })
+                }
+              >
+                <div className="flex justify-between items-center ">
+                  <FaInfoCircle className="text-xl ml-3 " />
+                  <p className=""> درباره ما</p>
+                </div>
+                <FiArrowLeft
+                  className={`${
+                    showFlash.aboutMe
+                      ? "opacity-100 animate-arrow-left"
+                      : "opacity-0"
+                  } text-blue-700 `}
+                />
+              </div>
+              <div
+                className="flex justify-between items-center py-4 px-2 text-slate-600 w-full hover:bg-stone-100 rounded-lg "
+                onMouseEnter={() =>
+                  setShowFlash({
+                    ...showFlash,
+                    aboutMe: false,
+                    instagram: false,
+                    telegram: true,
+                    products: false,
+                    freeLearn: false,
+                  })
+                }
+              >
+                <div className="flex justify-between items-center ">
+                  <FaTelegram className="text-xl ml-3" />
+                  <p className=""> کانال تلگرام</p>
+                </div>
+                <FiArrowLeft
+                  className={`${
+                    showFlash.telegram
+                      ? "opacity-100 animate-arrow-left"
+                      : "opacity-0"
+                  } text-blue-700`}
+                />
+              </div>
+              <div
+                className="flex justify-between items-center py-4 px-2 text-slate-600 w-full hover:bg-stone-100 rounded-lg"
+                onMouseEnter={() =>
+                  setShowFlash({
+                    ...showFlash,
+                    aboutMe: false,
+                    instagram: true,
+                    telegram: false,
+                    products: false,
+                    freeLearn: false,
+                  })
+                }
+              >
+                <div className="flex justify-between items-center ">
+                  <FaInstagramSquare className="text-xl ml-3" />
+                  <p className="">صفحه اینستاگرام</p>
+                </div>
+                <FiArrowLeft
+                  className={`${
+                    showFlash.instagram
+                      ? "opacity-100 animate-arrow-left"
+                      : "opacity-0"
+                  } text-blue-700  `}
+                />
+              </div>
             </div>
           </li>
         </ul>
       </div>
-      <div className="flex items-center justify-between text-lg  text-slate-600 max-[495px]:text-sm ">
+      <div className="flex items-center justify-between text-lg  text-slate-600 max-[495px]:text-sm h-full">
         <div className="max-[546px]:hidden  px-4 py-1 mx-1 cursor-pointer hover:text-blue-700">
           ورود
         </div>
@@ -93,7 +295,15 @@ const Header = () => {
   );
 };
 
-const Dropshot = ({ setDropshot, dropshot }) => {
+const Dropshot = ({
+  setDropshot,
+  dropshot,
+  setDropdownLearn,
+  setDropdownContactUs,
+  dropdownLearn,
+  dropdownContactUs,
+}) => {
+  console.log(dropdownLearn);
   return (
     <div
       className={`${
@@ -103,7 +313,7 @@ const Dropshot = ({ setDropshot, dropshot }) => {
       <div
         className={`${
           dropshot ? "w-full opacity-50 " : "w-0 opacity-0 "
-        }  bg-slate-900 h-screen  transition-all duration-400 absolute  z-20 top-0 right-0 `}
+        }  bg-slate-900 h-screen  transition-all duration-0 absolute  z-20 top-0 right-0 `}
       ></div>
       <div
         className={`${
@@ -123,33 +333,99 @@ const Dropshot = ({ setDropshot, dropshot }) => {
               </div>
             </div>
             <FiX
-              className="text-[1.8rem] font-bold text-slate-600"
+              className="text-[1.8rem] font-bold text-slate-600 cursor-pointer"
               onClick={() => setDropshot(false)}
             />
           </div>
-          <ul className="text-lg  flex text-slate-600 flex-col p-6 w-full" >
-            <li className=" w-full  py-3 my-3 cursor-pointer hover:text-blue-700">
-              اخبار
-            </li>
-            <li className=" w-full  py-3 my-3 cursor-pointer hover:text-blue-700">
-              مارکت
-            </li>
-            <li className=" w-full  py-3 my-3 cursor-pointer hover:text-blue-700">
-              تحلیل روزانه
-            </li>
-            <li className=" w-full  py-3 my-3 cursor-pointer hover:text-blue-700">
-              NFT & Airdrop
-            </li>
-            <li className=" w-full  py-3 my-3 cursor-pointer hover:text-blue-700">
-              <div className="flex items-center justify-between">
-                <h2>اموزش</h2>
-                <FaCaretDown className="text-sm mr-2" />
+          <ul className="text-lg  flex text-slate-600 flex-col p-6 w-full">
+            <li className=" w-full  py-3  cursor-pointer hover:text-blue-700 ">
+              <div className="flex items-center justify-start">
+                <HiNewspaper className="text-xl ml-3" />
+                <h2>اخبار</h2>
               </div>
             </li>
             <li className=" w-full  py-3 my-3 cursor-pointer hover:text-blue-700">
-              <div className="flex items-center justify-between">
-                <h2> ارتباط با ما</h2>
-                <FaCaretDown className="text-sm mr-2" />
+              <div className="flex items-center justify-start">
+                <HiChartBar className="text-xl ml-3 " />
+                <h2>مارکت</h2>
+              </div>{" "}
+            </li>
+            <li className=" w-full  py-3 my-3 cursor-pointer hover:text-blue-700">
+              <div className="flex items-center justify-start">
+                <BiAnalyse className="text-xl ml-3" />
+                <h2>تحلیل روزانه</h2>
+              </div>
+            </li>
+            <li className=" w-full  py-3 my-3 cursor-pointer hover:text-blue-700">
+              <div className="flex items-center justify-start">
+                <FaBtc className="text-xl ml-3" />
+                <h2>NFT & Airdrop</h2>
+              </div>
+            </li>
+            <li className="  flex flex-col items-start justify-start w-full  py-3 my-0 cursor-pointer hover:text-blue-700">
+              <div
+                className="flex items-center justify-between w-full"
+                onClick={() => setDropdownLearn(!dropdownLearn)}
+              >
+                <div className="flex items-center justify-start">
+                  <HiAcademicCap className="text-xl ml-3" />
+                  <h2>اموزش</h2>
+                </div>
+                <FaCaretDown className={`${dropdownLearn?'rotate-180':''} text-lg mr-2 transition-all duration-500`} />
+              </div>
+              <div
+                className={`${
+                  dropdownLearn ? "h-[8rem]  opacity-100  " : " h-0 opacity-0 -z-50 "
+                }  w-full transition-all ease-in-out duration-700    pt-2  `}
+              >
+                  <div className="flex justify-between items-center py-4 px-2 text-slate-600 w-full hover:bg-stone-100 rounded-lg">
+                    <div className="flex justify-between items-center ">
+                      <FaInfoCircle className="text-xl ml-3 " />
+                      <p className=""> اموزش های رایگان </p>
+                    </div>
+                  </div>
+                  <div className="flex justify-between items-center py-4 px-2 text-slate-600 w-full hover:bg-stone-100 rounded-lg">
+                    <div className="flex justify-between items-center ">
+                      <FaProductHunt className="text-xl ml-3" />
+                      <p className="">صفحه محصولات</p>
+                    </div>
+                   
+                  </div>
+              </div>
+            </li>
+            <li className="relative flex flex-col items-start justify-start w-full  pt-3 my-0 cursor-pointer hover:text-blue-700 ">
+              <div className="flex items-center justify-between w-full " onClick={()=>setDropdownContactUs(!dropdownContactUs)}>
+                <div className="flex items-center justify-start">
+                  <HiChatBubbleLeftRight className="text-xl ml-3" />
+                  <h2>ارتباط با ما</h2>
+                </div>
+                <FaCaretDown className={`${dropdownContactUs?'rotate-180':''} text-lg mr-2 transition-all duration-500`} />
+              </div>
+              <div
+                className={`${
+                  dropdownContactUs
+                    ? "h-[11rem]  opacity-100 "
+                    : " h-0  opacity-0 "
+                } w-full transition-all ease-in-out duration-700 pt-4`}
+              >
+                <div className="flex justify-between items-center py-4 px-2 text-slate-600 w-full  hover:bg-stone-100 rounded-lg">
+                  <div className="flex justify-between items-center ">
+                    <FaInfoCircle className="text-xl ml-3 " />
+                    <p className=""> درباره ما</p>
+                  </div>
+                </div>
+                <div className="flex justify-between items-center py-4 px-2 text-slate-600 w-full hover:bg-stone-100 rounded-lg ">
+                  <div className="flex justify-between items-center ">
+                    <FaTelegram className="text-xl ml-3" />
+                    <p className=""> کانال تلگرام</p>
+                  </div>
+                </div>
+                <div className="flex justify-between items-center py-4 px-2 text-slate-600 w-full hover:bg-stone-100 rounded-lg">
+                  <div className="flex justify-between items-center ">
+                    <FaInstagramSquare className="text-xl ml-3" />
+                    <p className="">صفحه اینستاگرام</p>
+                  </div>
+                </div>
               </div>
             </li>
           </ul>
@@ -161,7 +437,6 @@ const Dropshot = ({ setDropshot, dropshot }) => {
 
 function ChangeThem({ them, setThem }) {
   const [enabled, setEnabled] = useState(false);
-  console.log(them);
   return (
     <Switch
       checked={enabled}
@@ -189,5 +464,4 @@ function ChangeThem({ them, setThem }) {
     </Switch>
   );
 }
-// min-w-[18.9rem] min-[400px]:min-w-[23rem]
 export default Header;
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,20 @@ module.exports = {
       fontFamily: {
         sans: ["iranyekan"],
       },
+      animation: {
+        'arrow-left': 'arrow-left 1.5s ease-out infinite',
+      },
+      keyframes: {
+        'arrow-left': {
+          '0%': { transform: 'translateX(5px)' },
+          '50%': { transform: 'translateX(-10px)' },
+          '100%': {  transform: 'translateX(5px)' },
+        },
+      },
+      transitionProperty: {
+        'height': 'height',
+        'spacing': 'margin, padding',
+      },
     },
   },
   plugins: [],
