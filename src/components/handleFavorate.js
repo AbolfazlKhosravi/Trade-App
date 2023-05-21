5af823182378089b12ccb29d0776efe2ff11a470
AbolfazlKhosravi <abolfazlkhosravi.2003jun@gmail.com>
2023-05-21 15:30:42 +0330

    develop for when Request again when the previous request was not successful

--- src/components/handleFavorate.js
+++ src/components/handleFavorate.js
@@ -1,34 +1,41 @@
-import {FaRegHeart, FaHeart} from "react-icons/fa";
-import {
-  addFavorite,
-  deleteFavorite,
-} from "../features/products/favoritesSlice";
+import {FaRegHeart, FaHeart, FaRedoAlt} from "react-icons/fa";
+import {addFavorite, deleteFavorite} from "../features/products/favoritesSlice";
 import {useDispatch, useSelector} from "react-redux";
 import lodingSvg from "../assets/images/loading.svg";
 
 const HandleFavorite = ({product}) => {
-  const {
-    favorite,
-    error,
-    clickedShowLoding,
-    clickedShowError,
-    favorites,
-  } = useSelector((state) => state.favorites);
+  const {favorite, error, clickedShowLoding, clickedShowError, favorites} =
+    useSelector((state) => state.favorites);
+    
   const isClickedLoding = clickedShowLoding.find((cli) => cli === product.id);
   const isClickedError = clickedShowError.find((cli) => cli === product.id);
-
   const dispatch = useDispatch();
+  const isFavorite = favorites.find((fav) => fav.id === product.id);
+
+  const handleRetry = () => {
+    if (!isFavorite) {
+      dispatch(addFavorite(product));
+    } else {
+      dispatch(deleteFavorite(product.id));
+    }
+  };
+
   if (isClickedLoding) {
     return <img className="w-6 h-6" src={lodingSvg} alt="svg loading" />;
   }
+
   if (isClickedError) {
     return (
-      <div className="flex justify-center text-[.7rem] text-red-500">
-        <span className="text-blue-600 ml-1">خطا</span>: {error?error:"Network Error"}
+      <div
+        className="flex justify-center text-[.7rem] text-red-500 cursor-pointer"
+        onClick={handleRetry}>
+        <span className="text-blue-600 ml-1">خطا</span>:{" "}
+        {error ? error : "Network Error"}{" "}
+        <FaRedoAlt className="text-blue-600 mt-[1.5px] mr-2" />
       </div>
     );
   }
-  const isFavorite = favorites.find((fav) => fav.id === product.id);
+
   return (
     <div>
       {isFavorite ? (
