18a69a765522ab339a3f92e9eb54e8ddca172f0d
AbolfazlKhosravi <abolfazlkhosravi.2003jun@gmail.com>
2023-06-17 22:30:13 +0330

    develope footer site

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,72 @@
+import {
+  FaTwitter,
+  FaLinkedin,
+  FaInstagram,
+  FaYoutube,
+} from "react-icons/fa";
+import iconeBrand from "../assets/images/iconeBrand.svg";
 const Footer = () => {
   return (
-    <div className="">
-      <div className="2xl:container mx-auto  w-full h-16  "></div>
-    </div>
+    <footer className="bg-[#f4f2ed] flex flex-col w-full h-full  sticky dark:bg-slate-800">
+      <div className="flex justify-between items-center w-full border-b-2  pb-4 py-8 px-4 md:px-8 ">
+        <div className="flex justify-between items-center">
+          <div className="flex items-center justify-between ml-8">
+            <img src={iconeBrand} alt="iconeBrand" className="w-9 h-9" />
+            <div className="flex flex-col items-center justify-center mx-3">
+              <h1 className=" text-2xl text-blue-700 font-extrabold dark:text-white">
+                {" "}
+                ترید هوم
+              </h1>
+              <p className="  text-[.7rem] text-blue-700 dark:text-slate-300">
+                سایت اموزش ترید
+              </p>
+            </div>
+          </div>
+        </div>
+        <h3 className="hidden lg:flex text-blue-500 text-xl">آسان نگهش دار</h3>
+        <a to="/about-us" className="text-xl text-blue-500">
+          درباره ما
+        </a>
+      </div>
+      <div className="flex items-start justify-between mt-8 mx-4 md:mx-8 ">
+        <div className="flex justify-between items-start w-[14rem] md:w-1/2  xl:w-1/4">
+        <div className="flex flex-col">
+            <h3 className="text-2xl text-blue-600">خدمات</h3>
+            <div className="mt-5">
+                <p className="mb-2 text-slate-500">فروشگاه</p>
+                <p className="mb-2 text-slate-500">اموزش ها</p>
+                <p className="mb-2 text-slate-500">مارکت</p>
+                <p className="mb-2 text-slate-500">تحلیل روزانه</p>
+            </div>
+        </div>
+        <div className="flex flex-col">
+            <h3 className="text-md text-blue-600 mt-1 ">پیوستن به ما</h3>
+            <div className="mt-[1.4rem]">
+                <p className="mb-2 text-slate-500">ثبت نام</p>
+                <p className="mb-2 text-slate-500">ورود</p>
+            </div>
+        </div>
+        </div>
+       
+        <div className="">
+          <a href="# ">
+            <FaTwitter className="text-3xl mb-4 text-blue-500" />
+          </a>
+         
+          <a href="# ">
+            <FaInstagram className="text-3xl mb-4 text-red-500" />
+          </a>
+          <a href="# ">
+            <FaLinkedin className="text-3xl mb-4 text-blue-500" />
+          </a>
+         
+          <a href="# ">
+            <FaYoutube className="text-3xl mb-4 text-red-500" />
+          </a>
+        </div>
+      </div>
+       <p className="text-[.7rem] opacity-30 px-3 md:px-4 py-3 dark:text-white">AKH 0.0.1</p>
+    </footer>
   );
 };
 
