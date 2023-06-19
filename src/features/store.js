ac19f2130912c9df71da00454426c416f9fc4a7c
AbolfazlKhosravi <abolfazlkhosravi.2003jun@gmail.com>
2023-06-19 22:39:58 +0330

    create coursesSlice and Devolope it

--- src/features/store.js
+++ src/features/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit'
 import productsReducer from './products/productsSlice' 
 import favoritesReducer from './products/favoritesSlice'
 import cartReducer from './products/cartSlice'
+import coursesSlice from './products/coursesSlice'
 const store = configureStore({
     reducer: {
         products:productsReducer,
         favorites:favoritesReducer,
-        cart:cartReducer
+        cart:cartReducer,
+        courses:coursesSlice
     },
 })
 
