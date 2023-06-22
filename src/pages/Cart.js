5bb497bdb1477c8157f48303e3719aef8ed27ce8
AbolfazlKhosravi <abolfazlkhosravi.2003jun@gmail.com>
2023-06-22 12:33:59 +0330

    develope page cart

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,26 +1,70 @@
 import {useDispatch, useSelector} from "react-redux";
 import Layout from "../layout/layout";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import {fetchFavorite} from "../features/products/favoritesSlice";
 import {deleteCart, fetchCart} from "../features/products/cartSlice";
 import lodingSvg from "../assets/images/loading.svg";
 import HandleFavorateAll from "../components/HandleFavorateAll";
 import StarRating from "../components/StarRating";
-import {FaTimes,FaRedoAlt} from "react-icons/fa";
+import {FaTimes, FaRedoAlt, FaReply, FaSadTear ,FaPlusCircle ,FaMinusCircle } from "react-icons/fa";
+import {useNavigate} from "react-router-dom";
+import {toast} from "react-hot-toast";
 
 const Cart = () => {
   const dispatch = useDispatch();
-  const cartData = useSelector((state) => state.cart);
-  console.log(cartData);
-  // const navigate = useNavigate();
+  const [shouldExecuteCode, setShouldExecuteCode] = useState(false);
+  const {
+    checkedAddedToThecard,
+    product,
+    errorCart,
+    checkedRemovedToThecard,
+    loadingAll,
+    errorAll,
+    cart,
+    clickedShowLoding,
+    clickedShowError,
+  } = useSelector((state) => state.cart);
+
+  const navigate = useNavigate();
 
   useEffect(() => {
     dispatch(fetchFavorite());
     dispatch(fetchCart());
   }, [dispatch]);
 
+  useEffect(() => {
+    if (!shouldExecuteCode) {
+      return;
+    }
+
+    if (product && checkedAddedToThecard === product.id) {
+      toast.success(`به سبد خرید اضافه شد`);
+    }
+    if (!errorCart && checkedRemovedToThecard) {
+      toast.success(`از سبد خرید حذف شد`);
+    }
+    if (errorCart && checkedAddedToThecard) {
+      toast.error(`به سبد خرید اضافه نشد`);
+    }
+    if (errorCart && checkedRemovedToThecard) {
+      toast.error(`از سبد خرید حذف نشد`);
+    }
+  }, [
+    shouldExecuteCode,
+    checkedAddedToThecard,
+    product,
+    errorCart,
+    checkedRemovedToThecard,
+  ]);
+
+  useEffect(() => {
+    if (!shouldExecuteCode) {
+      setShouldExecuteCode(true);
+    }
+  }, [shouldExecuteCode]);
+
   const HandleShoweProductsCart = () => {
-    if (cartData.loadingAll) {
+    if (loadingAll) {
       return (
         <div className="w-full my-12 flex justify-center">
           <img className="w-20  " src={lodingSvg} alt="loding Svg" />
@@ -28,23 +72,39 @@ const Cart = () => {
       );
     }
 
-    if (cartData.errorAll) {
+    if (errorAll) {
       return (
         <div className="w-full my-[4.5rem] flex justify-center text-xl  text-red-500">
-          <span className="text-blue-600 ml-4 ">خطا</span> : {cartData.errorAll}
+          <span className="text-blue-600 ml-4 ">خطا</span> : {errorAll}
         </div>
       );
     }
+    if (cart.length === 0) {
+      return (
+        <div className="flex items-center justify-center w-full mt-20 ">
+          <p className=" text-slate-600 dark:text-slate-400">
+            سبد خرید شما خالی است
+          </p>
+          <FaSadTear
+            onClick={() => navigate("/")}
+            className="text-blue-500 mr-4 text-3xl hover:scale-105 transition-all cursor-pointer"
+          />
+        </div>
+      );
+    }
+
     return (
       <div className="py-4 px-2 flex flex-col">
         {" "}
-        {cartData.cart.map((product) => {
-          const isClickedLoding = cartData.clickedShowLoding.find(
+        {cart.map((product) => {
+          console.log(product.name.length);
+          const isClickedLoding = clickedShowLoding.find(
             (cli) => cli === product.id
           );
-          const isClickedError = cartData.clickedShowError.find(
+          const isClickedError = clickedShowError.find(
             (cli) => cli === product.id
           );
+
           return (
             <div
               key={product.id}
@@ -61,7 +121,7 @@ const Cart = () => {
               </div>
               <div className="flex flex-col items-start justify-start h-full w-2/4 pr-4  mt-2">
                 <div className="flex w-full items-center justify-between">
-                  <p className="text-[1.2rem] text-slate-700 font-extrabold">
+                  <p className={` text-slate-700 font-extrabold ${product.name.length>=20? "text-[.8rem]":"text-[1.1rem]"}`}>
                     {product.name}
                   </p>
                   {isClickedLoding ? (
@@ -70,7 +130,12 @@ const Cart = () => {
                       src={lodingSvg}
                       alt="svg loading"
                     />
-                  ) : isClickedError ?<FaRedoAlt onClick={()=>dispatch(deleteCart(product.id))} className=" text-red-500 cursor-pointer " /> : (
+                  ) : isClickedError ? (
+                    <FaRedoAlt
+                      onClick={() => dispatch(deleteCart(product.id))}
+                      className=" text-red-500 cursor-pointer "
+                    />
+                  ) : (
                     <FaTimes
                       onClick={() => dispatch(deleteCart(product.id))}
                       className="text-lg text-slate-500 cursor-pointer hover:scale-105 transition-all"
@@ -80,20 +145,27 @@ const Cart = () => {
                 <div className="text-[.8rem] mb-4">
                   <StarRating rating={product.rate} />
                 </div>
+                <div className="flex my-3 w-full items-center justify-between">
+                  <div className="flex items-center ">
+                    <FaPlusCircle className="text-[1.3rem] ml-2"/>
+                    <FaMinusCircle className="text-[1.3rem] text-red-500"/>
+                  </div>
+                  <p className="font-bold text-white bg-red-500 rounded-full w-[1.35rem] h-[1.35rem] flex justify-center  text-lg "> {product.quantity} </p>
+                </div>
                 {product.price === product.discountedPrice ? (
-                  <p className="text-lg text-slate-700 font-extrabold mt-[4.7rem]">
+                  <p className="text-[1.05rem] text-slate-700 font-extrabold mt-6">
                     {product.price.toLocaleString("fa")}
                     <span className="mr-2 font-bold text-blue-500">تومان</span>
                   </p>
                 ) : (
-                  <div className="flex flex-col">
-                    <p className="text-lg text-slate-700 font-extrabold mt-14">
+                  <div className="flex flex-col mt-2">
+                    <p className="text-[1.05rem] text-slate-700 font-extrabold ">
                       {product.discountedPrice.toLocaleString("fa")}
                       <span className="mr-2 font-bold text-blue-500">
                         تومان
                       </span>
                     </p>
-                    <p className="text-[.85rem] text-slate-500 line-through">
+                    <p className="text-[.78rem] text-slate-500 line-through mt-0">
                       {product.price.toLocaleString("fa")} تومان
                     </p>
                   </div>
@@ -109,9 +181,15 @@ const Cart = () => {
   return (
     <Layout>
       <main className="2xl:container mx-auto flex flex-col items-start justify-start ">
-        <h1 className="py-6 px-2 text-xl font-bold text-blue-500">
-          سبد خرید شما
-        </h1>
+        <div className="flex items-center w-full justify-between px-2">
+          <h1 className="py-6  text-xl font-bold text-blue-500">
+            سبد خرید شما
+          </h1>
+          <FaReply
+            onClick={() => navigate("/")}
+            className="text-slate-600 text-lg hover:scale-105 transition-all cursor-pointer"
+          />
+        </div>
         <div className=" w-full flex justify-center text-xl font-bold text-blue-500">
           {HandleShoweProductsCart()}
         </div>
