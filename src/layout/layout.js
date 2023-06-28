5ffedfa500b92b72d1825634315e3bab97e1de34
AbolfazlKhosravi <abolfazlkhosravi.2003jun@gmail.com>
2023-06-28 20:48:06 +0330

    writte  better Code and prettierr in AboutMe

--- src/layout/layout.js
+++ src/layout/layout.js
@@ -3,13 +3,22 @@ import Footer from "../components/Footer";
 import {useLocation} from "react-router-dom";
 import {useEffect} from "react";
 import {animateScroll as scroll} from "react-scroll";
+import { fetchFavorite } from "../features/products/favoritesSlice";
+import { useDispatch } from "react-redux";
+import { fetchCart } from "../features/products/cartSlice";
 
 const Layout = ({children}) => {
   const {pathname} = useLocation();
+  const dispatch = useDispatch();
 
   useEffect(() => {
     scroll.scrollToTop();
   }, [pathname]);
+
+  useEffect(() => {
+    dispatch(fetchFavorite());
+    dispatch(fetchCart());
+  }, [dispatch]);
 
   return (
     <div className="min-h-screen bg-[#F2F0F0] dark:bg-slate-900 flex flex-col">
--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -1,9 +1,5 @@
-import {Navigate, useNavigate} from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 import Layout from "../layout/layout";
-import {useDispatch} from "react-redux";
-import {fetchFavorite} from "../features/products/favoritesSlice";
-import {fetchCart} from "../features/products/cartSlice";
-import {useEffect} from "react";
 import {
   FaTwitter,
   FaLinkedin,
@@ -16,13 +12,7 @@ import {
 import convertToPersianNumber from "../utils/ConverToPersianNumber";
 
 const AboutMe = () => {
-  const dispatch = useDispatch();
   const navigate = useNavigate();
-
-  useEffect(() => {
-    dispatch(fetchFavorite());
-    dispatch(fetchCart());
-  }, [dispatch]);
 
   return (
     <Layout>
@@ -39,14 +29,20 @@ const AboutMe = () => {
         </div>
         <div className="bg-slate-50 mx-8 rounded-3xl p-4 translate-y-32 absolute md:mx-20 md:p-6 dark:bg-slate-800">
           <section className="">
-            <h2 className="text-slate-700 font-bold text-xl md:text-[1.4rem] dark:text-slate-400 md:mb-1"> درباره من</h2>
+            <h2 className="text-slate-700 font-bold text-xl md:text-[1.4rem] dark:text-slate-400 md:mb-1">
+              {" "}
+              درباره من
+            </h2>
             <p className="text-slate-500 p-2 text-[.95rem] font-medium leading-7 md:text-lg dark:text-slate-500">
               من یک فرانت اند کار جونیور 20 ساله ساکن شیراز , علاقه مند به
-              برنامه نویسی و حوزه فرانت هستم  .
+              برنامه نویسی و حوزه فرانت هستم .
             </p>
           </section>
           <section className="">
-            <h2 className="text-slate-700 font-bold text-xl my-3 dark:text-slate-400"> مهارت ها</h2>
+            <h2 className="text-slate-700 font-bold text-xl my-3 dark:text-slate-400">
+              {" "}
+              مهارت ها
+            </h2>
             <div className="text-slate-500 px-2  font-medium  font-mono text-[1rem] flex flex-wrap justify-between items-center leading-9 dark:text-slate-500">
               <p className="mx-1">HTML 5</p>
               <p className="mx-1">CSS</p>
@@ -93,18 +89,26 @@ const AboutMe = () => {
             <div className="flex flex-col w-full items-start justify-center lg:mb-2 ">
               <div className="flex items-center">
                 <FaPhoneSquare className="ml-2 text-[1.8rem] hover:scale-105 transition-all text-blue-500" />
-                <p dir="ltr" className="text-slate-700 text-sm dark:text-slate-500">{convertToPersianNumber("0990 618 2988")}</p>
+                <p
+                  dir="ltr"
+                  className="text-slate-700 text-sm dark:text-slate-500">
+                  {convertToPersianNumber("0990 618 2988")}
+                </p>
               </div>
               <div className="flex items-center my-4">
                 <FaEnvelope className="ml-2 text-[1.7rem] hover:scale-105 transition-all text-slate-700" />
-                <p dir="ltr" className="text-slate-700 text-sm dark:text-slate-500">abolfazlkhosravi.2003jun@gmail.com</p>
+                <p
+                  dir="ltr"
+                  className="text-slate-700 text-sm dark:text-slate-500">
+                  abolfazlkhosravi.2003jun@gmail.com
+                </p>
               </div>
             </div>
           </section>
           <section className=" flex justify-between items-center">
             <button
-              className="py-2 bg-blue-500 rounded-2xl text-slate-100 font-bold px-3 hover:scale-105 transition-all cursor-pointe"
-              onClick={() => Navigate("/login")}>
+              className="py-2 bg-blue-500 rounded-2xl text-slate-100 font-bold px-12 hover:scale-105 transition-all cursor-pointe"
+              onClick={() => navigate("/login")}>
               ثبت نام
             </button>
             <FaReply
